7ff25880e95a30012fce99d6b9f094742c6140ad
Sidharth <tornadoalert@gmail.com>
2017-12-14 10:55:12 +0530

    Forms work

--- land/views.py
+++ land/views.py
@@ -2,29 +2,45 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, FormView
 from .forms import CreateForm, TransferForm, RegisterForm
+from django.contrib import messages 
+
 # Create your views here.
 
-def create(request):
-    return render(request,'land/create.html')
-
-def transfer(request):
-    return render(request,'land/transfer.html')
-
-def register(request):
-    return render(request,'land/register.html')
+def success(request):
+    return render(request,'land/success.html')
 
 class CreateView(FormView):
     form_class = CreateForm
     template_name = 'land/create.html'
-    success_url = '/success'
+    success_url = '/create'
     def form_valid(self,form):
-        # Post on IPDB
-        return super().form_valid(self,form)
+        
+        if form.is_valid():
+            f = form.cleaned_data
+            # Post on IPDB
+            # Register with database
+            messages.success(self.request, f)
+        return super().form_valid(form)
 
 class TransferView(FormView):
     form_class = TransferForm
     template_name = 'land/transfer.html'
+    success_url = '/transfer'
+    def form_valid(self,form):
+        # Post on IPDB
+        if form.is_valid():
+            f = form.cleaned_data
+        messages.success(self.request, f) 
+        return super().form_valid(form)
 
 class RegisterView(FormView):
     form_class = RegisterForm
     template_name = 'land/register.html'
+    success_url = '/success'
+    def form_valid(self,form):
+        
+        if form.is_valid():
+            f = form.cleaned_data
+            # Register to DataBase
+            messages.success(self.request, f)
+        return super().form_valid(form)
