33ad4b76e6258811fc69030a79ef6c87733435f6
Sidharth <tornadoalert@gmail.com>
2017-12-14 18:05:39 +0530

    no static folder scan

--- blockchainland/settings.py
+++ blockchainland/settings.py
@@ -128,9 +128,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
